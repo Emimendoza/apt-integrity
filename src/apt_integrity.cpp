ce8aceb8c6f138450a6a12ab719108262379dd94
mephi <emiliomendozareyes@gmail.com>
2022-09-14 16:37:47 -0500

    Added lock system compatible with apt-mirror and parsed app-mirror config files

--- src/apt_integrity.cpp
+++ src/apt_integrity.cpp
@@ -3,9 +3,13 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <sys/file.h>
+#include <unistd.h>
+#include <openssl/sha.h>
 
 using namespace apt_integrity;
 
+// Helper functions
 std::vector<std::string> apt_int::splitString(const std::string& str)
 {
     std::stringstream ss(str);
@@ -16,8 +20,12 @@ std::vector<std::string> apt_int::splitString(const std::string& str)
         tokens.push_back(temp);
     }
     return tokens;
-}
 
+}
+std::string apt_int::sha256(const std::string &path) {
+    return std::string();
+}
+// Main Functions
 void apt_int::readSourceFile() {
     std::vector<source> sources;
     // std::ifstream sourceList("/etc/apt/mirror.list");
@@ -26,12 +34,12 @@ void apt_int::readSourceFile() {
     while(sourceList)
     {
         std::getline(sourceList,line);
+        std::vector<std::string> tokens = splitString(line);
         try
         {
             if (line.starts_with("deb"))
             {
                 source currentSource;
-                std::vector<std::string> tokens = splitString(line);
                 int currToken = 2;
                 if(tokens[1].starts_with("[arch="))
                 {
@@ -52,6 +60,17 @@ void apt_int::readSourceFile() {
                 }
                 this->_sources.push_back(currentSource);
             }
+            if (line.starts_with("set"))
+            {
+                if (tokens[1] == "base_path")
+                    _basePath = std::string(tokens[2]);
+                if (tokens[1] == "mirror_path")
+                    _mirrorPath = std::string(tokens[2]);
+                if (tokens[1] == "var_path")
+                    _varPath = std::string(tokens[2]);
+                if (tokens[1] == "nthreads")
+                    _threads = std::stoi(tokens[2]);
+            }
         }
         catch (...)
         {
@@ -61,8 +80,42 @@ void apt_int::readSourceFile() {
     }
 }
 
+void apt_int::fixPaths()
+{
+    if(_mirrorPath.starts_with("$base_path"))
+        _mirrorPath = _basePath + _mirrorPath.substr(10);
+    if(_varPath.starts_with("$base_path"))
+        _varPath = _basePath + _varPath.substr(10);
+}
+
+void apt_int::setLock()
+{
+    _lockfd = open((_varPath+"/apt-mirror.lock").c_str(),O_CREAT|O_RDONLY,0666);
+     if (flock(_lockfd, LOCK_EX | LOCK_NB) != 0)
+     {
+         std::cerr << "Couldn't get lock on lock file, maybe apt-mirror is running?" << '\n';
+         exit(1);
+     }
+}
+
+void apt_int::releaseLock()
+{
+    if (flock(_lockfd, LOCK_UN)!=0 || close(_lockfd) != 0)
+    {
+        std::cerr << "Error Unlocking file" << "\n";
+    }
+    if(unlink((_varPath+"/apt-mirror.lock").c_str())!=0)
+    {
+        std::cerr << "Error deleting lock file" << "\n";
+    }
+
+}
+
 int apt_int::main()
 {
     readSourceFile();
+    fixPaths();
+    setLock();
+    releaseLock();
     return 0;
 }
--- src/apt_integrity.h
+++ src/apt_integrity.h
@@ -20,13 +20,21 @@ namespace apt_integrity
         int main();
     private:
         // Variables
+        int _threads;
+        int _lockfd;
         std::vector<source> _sources;
-        // Functions
+        std::string _mirrorPath = "$base_path/mirror";
+        std::string _basePath = "/var/spool/apt-mirror";
+        std::string _varPath = "$base_path/var";
+        // Helper Functions
         static std::vector<std::string> splitString(const std::string& str);
+        static std::string sha256(const std::string& path);
+        // Main Functions
         void readSourceFile();
+        void fixPaths();
+        void setLock();
+        void releaseLock();
     };
 }
-
-
 
 #endif //APT_INTEGRITY_APT_INTEGRITY_H
